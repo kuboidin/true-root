9f136cb40ea77fb4e8a60da4a37256676990bc42
Vinay Kumar <kuboidin@gmail.com>
2025-05-05 16:30:46 +0530

    how to grow tab

--- src/json/vegetable/tomatoes.js
+++ src/json/vegetable/tomatoes.js
@@ -206,6 +206,161 @@ const plant = {
                     <><strong>Tip: </strong> Soil + Compost + Coco peat (Coco peat helps retain moisture for longer, if you do not have Coco peat it&apos;s Okay, just maintain the proper moisture).</>,
                     <><strong>Tip: </strong> See the <TabLink section="Seeding Process">Seeding process</TabLink> on this page for tips and things to care about.</>
                 ]
+            },
+            {
+                heading: "Day 0: Seeding",
+                steps: [
+                    "Sow seeds at 0.5 - 2.5 cm depth, just cover them with a thin layer of coco peat, compost, or soil and water them.",
+                    "If you are a beginner then use a seedling tray, seedling tray helps for easy movement, you can place them under sunlight during moving but move them inside during noon to protect them from harsh heat and so on.",
+                    "Keep in low sunlight or inside (not dark) if possible.",
+                    <><strong>Tip: </strong> See the <TabLink section="Seeding Process">Seeding process</TabLink> on this page for tips and temperature and environment conditions.</>
+                ]
+            },
+            {
+                heading: "Day 7: Sprouting",
+                steps: [
+                    "The Seed should sprout in a week.",
+                    "They need full light from day one of germination.",
+                    "Shift them to 1-2 hours of sunlight when you see 2 leaves, at this stage plants start looking for sunlight, if you do not put them in sunlight they will grow weak but longer and in multi-direction in search of sunlight (We don&apos;t want that).",
+                    "Seed provides all the required nutrients to support the life of the plant for 2 weeks.",
+                    <><strong>Tip: </strong> Do not add any fertilizer of any kind.</>,
+                    <><strong>Watering: </strong> Only water them if the top soil is dry, Avoid excess water at this stage, see the <TabLink section="Watering Schedule">watering guide</TabLink> for more information</>
+                ]
+            },
+            {
+                heading: "Day 15: First organic fertilizer",
+                steps: [
+                    "Provide liquid onion peel fertilizer in small dosages.",
+                    <>Onion peel fertilizer is rich in Potassium, Phosphorus, Nitrogen, Calcium, Iron, and Magnesium, see the <TabLink section="Nutritional Requirements">nutritional requirement</TabLink> section for more information.</>,
+                    "Add bone meal and vermicompost for nutrients.",
+                    <><strong>Tip: </strong> Do not use NPK, DAP, or chemical fertilizer at this stage, the plant will not be able to handle and die</>,
+                    <><strong>Watering: </strong> Only water them if the top soil is dry, Avoid excess water at this stage, see the <TabLink section="Watering Schedule">watering guide</TabLink> for more information</>
+                ]
+            },
+            {
+                heading: "Day 28: Shift to a larger pot (Only if you planted it in the seedling tray)",
+                steps: [
+                    "From seeding to 1 month, plants are very delicate, do not remove them or shift them, only perform shifting after 4 weeks of seeding. At this stage, seeds should have grown into small plants.",
+                    "Use at least a 12-18 inch deep pot or grow bag.",
+                    <><strong>Soil Mix: </strong> Use a well-draining potting mix enriched with compost. Ensure the pot has drainage holes.</>,
+                    "Add Neem powder (Neem Khali) to the soil to protect the roots from fungus and other germs.",
+                    "When you shift the plants from seedling to ground, grow bags or pots, make sure the roots are not exposed to air. Try not to disturb the roots.",
+                    "Be slow and gentle when removing from the seeding tray and planting in the pot or grow bags.",
+                    <>If roots are exposed to air or you see no soil with roots or if the plant is weathered (when you borrow from your friend), then <strong>sock the plant&apos;s root in water for 5-7 minutes</strong>. If plants are in very bad condition add 1 spoon of sugar in water and dip plants for 15 minutes. It will revive the plants and stabilize them.</>,
+                    "Perform shifting in the evening only and add plenty of water.",
+                    "Plants will/might show some stress like leaves withering after shifting, but they will recover soon if you have shifted correctly.",
+                    "To shift them in larger pots, put at least 40-50% of the plant stem under the soil, this will help in growing stronger roots.",
+                    "After seedlings have grown and become plants, they need at least 5-6 hours of sunlight every day.",
+                    <><strong>Tip: </strong> Add a layer of small stones or broken pots at the bottom of the grow bag or pots to improve drainage.</>
+                ]
+            },
+            {
+                heading: "Day 40: 2nd dose of fertilizer",
+                title: "At this point, plants are bulletproof and the road is going to get easy.",
+                steps: [
+                    "Add some organic fertilizers like peanuts, banana leaves, or similar fertilizers, and make sure plants are twice the size when shifted, only then provide the liquid fertilizer.",
+                    "You can also add seaweed, bone meal, eggshell powder, etc.",
+                    "Try to use water-soluble fertilizer at the beginning.",
+                    <><strong>Recommendation: </strong> We recommend not to use chemical fertilizers, They are harmful to the environment and you. Things grown organically taste much better and are healthier. But if you must then you can add them at this stage.</>
+                ]
+            },
+            {
+                heading: "Day 52: Grooming and caring (Pinching) of tomato plants",
+                title: <>10-12 days after 2nd dose of fertilizer, You will see the plants have grown rapidly. Let&apos;s perform Pinching. The act of <strong>pinching</strong> involves the <strong>removal of the growing point of a shoot along with a few leaves</strong>. About 1-2 cm of a growing shoot of a young plant is snapped off with fingers.</>,
+                steps: [
+                    "Make sure the main stem is thick enough and the plant has grown to twice the height and you see clusters of small growing leaves at the end of stems.",
+                    "This will help the plant to stop increasing height and focus on increasing the width and number of stems.",
+                    "Try to use water-soluble fertilizer at the beginning.",
+                    <><strong>Tip: </strong> Perform this pinching after every 10 days until you see flowers.</>,
+                    <><strong>Watering: </strong> Only water them if the top soil is dry, Avoid excess water at this stage, see the <TabLink section="Nutritional Requirements">watering guide</TabLink> for more information</>
+                ]
+            },
+            {
+                heading: "Day 64: Add Support",
+                title: "You will see plant has grown dense and bigger",
+                steps: [
+                    "If your pot or grow bag is smaller, now is the time to shift them into bigger pots or grow bags.",
+                    "Remove leaves and small stems near the soil, this will help the plant grow in length and protect it from soil-based diseases.",
+                    "Add support at this stage.",
+                    <><strong>Important: </strong> If you see that the plant is starting to grow flowers at this time, cut them.
+                        This will allow plants to grow further and yield more fruits at a later stage.
+                        If you don&apos;t cut them at this time, plant growth will stop and the plant will focus completely on flowers and fruits, this will result in small plants and low tomato yield.
+                    </>,
+                    <><strong>Tip: </strong> Perform this pinching after every 10 days until you see flowers.</>,
+                    <><strong>Watering: </strong> Only water them if the top soil is dry, Avoid excess water at this stage, see the watering guide for more information.</>,
+                    <><strong>Fertilizer: </strong> Until you don&apos;t see the small tomatoes, you will have to provide liquid fertilizer every 20 days.</>
+                ]
+            },
+            {
+                heading: "Day 87: Flowering & Remove Suckers",
+                title: <>You might start seeing the flowers at this stage <br/>
+                    A <strong>&quot;sucker&quot;</strong> is <strong>an extra shoot or leaves growing between the main stalk of the tomato plant and an established branch, often growing at a 45° angle from the plant</strong>.
+                </>,
+                steps: [
+                    "Clean the small leaves and thin stems up to 5-7 inches from the soil, only keep the thick branches.",
+                    "Remove suckers, (Suckers are leaves that are growing between two branches) they take plant&apos;s nutrients and we don&apos;t want them.",
+                    <><strong>Important: </strong> If/When you see flowers/fruits, at this time plants need more nutrients than ever.</>,
+                    <><strong>Tip: </strong> Remove suckers whenever you see them for a health plant.</>,
+                    <><strong>Tip: </strong> Increase the dosage of fertilizer.</>,
+                    <><strong>Watering: </strong> When you see fruit, then you must not let the soil dry, tomatoes may turn black if face water scarcity at this stage.</>,
+                    <><strong>Fertilizer: </strong> After you see bigger flowers and small fruits, add fertilizers containing micronutrients, such as seaweed (spray), onion peel liquid, and banana peel liquid, provide liquid fertilizer every 15 days, keep on changing, and try not to repeat the same fertilizer.</>
+                ]
+            },
+            {
+                heading: "Day 112: Tomatoes will start turning red",
+                title: "At this stage, tomatoes will start turning red and within a few days they will be ready to harvest.",
+                steps: [
+                    <><strong>Tip: </strong> Increase the dosage/frequency of fertilizer</>,
+                    <><strong>Watering: </strong> You must not let the soil dry, Water is very important</>,
+                    <><strong>Fertilizer: </strong> See the Fertilizer section for homemade and nutrient-rich fertilizers.</>
+                ]
+            },
+            {
+                heading: "Day 120: Fruit of hard work",
+                title: "Harvest the fruit of your hard work. This is not the end, Tomatoes will keep on provided with minimal watering and care at this time.",
+                steps: [
+                    <><strong>Tip: </strong> Remove weeds and keep looking for diseases, check the diseases section of how to find and fix the plant diseases.</>,
+                    <><strong>Tip: </strong> Keep providing fertilizer every 15-20 days.</>,
+                    <><strong>Watering: </strong> You must not let the soil dry, Water is very important.</>,
+                    <><strong>Fertilizer: </strong> See the Fertilizer section for homemade and nutrient-rich fertilizers.</>
+                ]
+            },
+            {
+                heading: "Continue",
+                title: <>Along with these steps and stages, there are a few things you need to perform throughout the process to grow tomatoes, each thing will take only 20-30 minutes a week of your time. <br/>
+                    Please see the related section for more information on each step, tips, and homemade recipes for fertilizers and plant care. <br/>
+                    Perform these steps every 3 weeks after 40 days of seeding (after shifting).
+                </>,
+                steps: [
+                    <><strong>Watering: </strong> <br/>
+                        Until you see the flowers on the plant, you will water them normally i.e. Only water them if the top soil is dry.<br/>
+                        If you water excessively, plants might get fungus, and attract more diseases, flowers will wither before time and fruiting will be delayed. <br/>
+                        When you see fruit, then you must not let the soil dry, tomatoes may turn black if face water scarcity at this stage. <br/>
+                        Check the <TabLink section="Watering Schedule">watering section</TabLink> for more information.
+                    </>,
+                    <><strong>Weeding & Caring: </strong> <br/>
+                        Remove weeds regularly. <br/>
+                        Perform Pinching. <br/>
+                        Remove Suckers. <br/>
+                        Check the <TabLink section="Weeding & Caring">weeding & caring</TabLink> section for more information.
+                    </>,
+                    <><strong>Fertilizer: </strong> <br/>
+                        Until when you don&apos;t see the small tomatoes, you will have to provide liquid fertilizer every 20 days.<br/>
+                        After you see bigger flowers and small fruits, add fertilizers containing micronutrients, such as seaweed (spray), onion peel liquid, and banana peel liquid, provide liquid fertilizer every 15 days, keep on changing, and try not to repeat the same fertilizer.<br/>
+                        Check the <TabLink section="Fertilizer Requirements">fertilizer section</TabLink> for more information.
+                    </>,
+                    <><strong>Diseases: </strong> <br/>
+                        Plants are prone to diseases, you need to look for signs and take care before they spread or ruin your hard work. <br/>
+                        There is very little chance of diseases in a controlled environment. <br/>
+                        Check the <TabLink section="Diseases - Know & Treat">diseases section</TabLink> for more information.
+                    </>,
+                    <><strong>Pollination: </strong> <br/>
+                        We believe in harmony, plants grow better with other plants and insects, try not to restrict your garden to birds, insects, bugs, and other organisms. <br/>
+                        Birds, and insects not only help in pollination but also improve the positivity of the environment, and help in nutrition circulation. <br/>
+                        Birds can eat small bugs and protect plants from diseases for free. <br/>
+                        Check the <TabLink section="Diseases - Know & Treat">Flowers Withering (No Pollination) section</TabLink> for more information.
+                    </>
+                ]
             }
         ]
     }
